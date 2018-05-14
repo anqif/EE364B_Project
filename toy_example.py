ebe99c2077f6066ccea4e6124ac7d3c5f2c064f3
Anqi Fu <anqi.fu.1987@gmail.com>
2018-05-13 20:02:49 -0700

    Fix problem constraints and change desired mask to smiley face

--- toy_example.py
+++ toy_example.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy as sp
+import scipy.linalg
 import matplotlib.pyplot as plt
 from cvxpy import *
 
@@ -8,7 +8,17 @@ m = 5
 n = 10
 I_high = 0.75
 I_low = 0.25
-S = np.random.randint(0, 2, size = (n,n))
+# S = np.random.randint(0, 2, size = (n,n))
+S = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+			  [0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+			  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
 # Generate light wavelengths
 p_light = []
@@ -16,15 +26,16 @@ for i in range(m):
 	data = np.random.randn(n,2)
 	comp = np.apply_along_axis(lambda args: [complex(*args)], 1, data)
 	p_light += [comp]
-Phis = [np.asmatrix(sp.linalg.circulant(p)) for p in p_light]
+Phis = [np.asmatrix(scipy.linalg.circulant(p)) for p in p_light]
+# Phis_split = [(np.real(Phi), np.imag(Phi)) for Phi in Phis]
 
 # Solve convex relaxation
 M = Semidef(n)
 obj = trace(M)
-expr = sum([diag(Phi.H*M*Phi) for Phi in Phis])
-# cons = [expr[S == 1] >= I_high, expr[S == 0] <= I_low]
-# prob = Problem(Minimize(obj), cons)
-prob = Problem(Minimize(obj))
+expr = sum([Phi.H*M*Phi for Phi in Phis])
+# expr = sum([Preal.T*M*Preal + Pimag.T*M*Pimag for Preal, Pimag in Phis_split])
+cons = [expr[S == 1] >= I_high, expr[S == 0] <= I_low]
+prob = Problem(Minimize(obj), cons)
 prob.solve()
 
 # Display resulting mask
