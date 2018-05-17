f3c7a1df306ca3048102b4b9f1585332634061de
anqif <anqif@stanford.edu>
2018-05-16 17:43:32 -0700

    Vectorize substrate before solving optimization problem

--- toy_example.py
+++ toy_example.py
@@ -5,21 +5,23 @@ from cvxpy import *
 
 np.random.seed(1)
 m = 5
-n = 25
+# n = 50
 I_high = 0.75
 I_low = 0.25
-S = np.random.randint(0, 2, size = (n,1))
-# S = np.random.randint(0, 2, size = (n,n))
-# S = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#			  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-#			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#			  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-#			  [0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
-#			  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-#			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+# S = np.random.randint(0, 2, size = (5,5))
+S = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+			  [0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+			  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+k,l = S.shape
+n = np.prod(S.shape)
+S_vec = S.flatten()   # Flatten into vector
 
 # Generate light wavelengths
 p_light = []
@@ -34,18 +36,20 @@ Phis = [np.asmatrix(scipy.linalg.circulant(p)) for p in p_light]
 M = Semidef(n)
 obj = trace(M)
 I = sum([diag(Phi.H*M*Phi) for Phi in Phis])
-# I = sum([Phi.H*M*Phi for Phi in Phis])
 # I = sum([Preal.T*M*Preal + Pimag.T*M*Pimag for Preal, Pimag in Phis_split])
-cons = [I[S == 1] >= I_high, I[S == 0] <= I_low]
+cons = [I[S_vec == 1] >= I_high, I[S_vec == 0] <= I_low]
 prob = Problem(Minimize(obj), cons)
-prob.solve()
-I_sol = np.real(I.value)
-print("MSE in Substrate: {}".format(np.linalg.norm(S - I_sol)**2/n))
+prob.solve("MOSEK")
 
-# Display resulting mask
+# Retrieve solution
+I_sol = np.real(I.value)
+S_sol = np.reshape(I_sol, (k,l))   # Reshape into 2-D image
+print("MSE in Substrate: {}".format(np.linalg.norm(S - S_sol)**2/n))
+
+# Display resulting substrate
 f, axarr = plt.subplots(1,2)
 axarr[0].imshow(S, cmap = "Greys", interpolation = "nearest")
 axarr[0].set_title("Desired Substrate")
-axarr[1].imshow(I_sol, cmap = "Greys", interpolation = "nearest")
+axarr[1].imshow(S_sol, cmap = "Greys", interpolation = "nearest")
 axarr[1].set_title("Solution Substrate")
 plt.show()
